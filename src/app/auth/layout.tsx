b0ef03d4c8fa259058fee8c1243ce710c05ddb71
Ryan Skolnick <rhskolnick@gmail.com>
2024-08-09 08:40:10 -0400

    Installed shadcnui

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -27,6 +27,26 @@ const Layout = async ({ children }: Props) => {
 				/>
 				{children}
 			</div>
+			<div className='hidden lg:flex flex-1 w-full max-h-full max-w-[4000px] overflow-hidden relative bg-cream flex-col pt-10 pl-24 gap-3'>
+				<h2 className='text-gravel md:text-4xl font-bold'>
+					Hi, I'm your AI powered sales assistant, Corinna!
+				</h2>
+				<p className='text-iridium md:text-sm mb-10'>
+					Corinna is capable of capturing lead information without a
+					form...{""}
+					<br />
+					something never done before
+				</p>
+				<Image
+					src='/images/app-ui.png'
+					alt='app image'
+					loading='lazy'
+					sizes='30'
+					className='absolute shrink-0 !w-[1600px] top-48'
+					width={0}
+					height={0}
+				/>
+			</div>
 		</div>
 	);
 };
